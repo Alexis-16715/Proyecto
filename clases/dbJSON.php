c1f2ad5fa78599bc8abbf41a81e32abcfde4c486
Dldja <l.rodriguezvaliente@gmail.com>
2017-11-21 16:58:29 -0300

    cambie dbJSON

--- clases/dbJSON.php
+++ clases/dbJSON.php
@@ -10,15 +10,29 @@ class dbJSON extends db {
   }
 
   public function traerTodos() {
-
+    $this->arch = "usuarios.json";
+      $usuariosJSON = explode(PHP_EOL, $this->$arch);
+		  array_pop($usuariosJSON);
+		  $usuariosFinal = [];
+    		foreach ($usuariosJSON as $usuario) {
+    			$usuariosFinal[] = json_decode($usuario, true);
+    		}
+    		return $usuariosFinal;
   }
 
   public function traerPorMail($email) {
-
-  }
+        $usuarios = traerTodos();
+      foreach ($usuarios as $unUsuario) {
+        if ($unUsuario['email'] == $mail) {
+          return $unUsuario;
+        }
+      }
+      return false;
+    }
 
   public function guardarUsuario(Usuario $usuario) {
-    
+    $usuarioFinal = json_encode($usuario);
+		file_put_contents('usuarios.json', $usuarioFinal . PHP_EOL, FILE_APPEND);
   }
 }
 
